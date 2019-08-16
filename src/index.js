e4410ddeb4bdf463db794ec3ad2fae3b48c38351
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-17 03:07:12 +0530

    Implemented subscription for new link created

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
 const resolvers = {
     Query,
     Mutation,
+    Subscription,
     User,
-    Link
+    Link,
   };
 
 // 3
