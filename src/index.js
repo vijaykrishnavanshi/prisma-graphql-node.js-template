819899fe0bb7920206ba15eaec76ed3060829495
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-17 00:04:34 +0530

    Added basic implementation of graphql server based on express

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+// 1
+const typeDefs = `
+    type Query {
+        info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
+    }
+`
+
+// 1
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+  
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        // 2
+        feed: () => links,
+    },
+    // 3
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
+    }
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
