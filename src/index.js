70f555b23029eaeb5048ec18871cc3ca62988a7f
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-17 02:23:25 +0530

    Modularise code in seperate resolvers

--- src/index.js
+++ src/index.js
@@ -1,54 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-// 1
-let idCount = links.length
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: (root, args, context, info) => {
-            return context.prisma.links();
-        },
-        link: (parent, args) => links.find(elem => elem.id === args.id),
-    },
-    Mutation: {
-        // 2
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        },
-        updateLink: (parent, args, context) => {
-            return context.prisma.updateLink({
-                data: {
-                    url: args.url,
-                    description: args.description,
-                },
-                where: {
-                    id: args.id,
-                }
-            })
-        },
-        deleteLink: (parent, args, context) => {
-            return context.prisma.deleteLink({
-                where: {
-                    id: args.id,
-                },
-            })
-        }
-    },
-};
+    Query,
+    Mutation,
+    User,
+    Link
+  };
 
 // 3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma },
-});
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
+})
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
