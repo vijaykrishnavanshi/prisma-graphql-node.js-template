ea7b52caaaac3b9c57c9c39200f8096ab7f343f8
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-17 10:47:38 +0530

    Implemented search in query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,17 @@
-function feed(parent, args, context, info) {
-    return context.prisma.links()
-  }
-  
-  module.exports = {
-    feed,
-  }
+async function feed(parent, args, context, info) {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where
+  })
+  return links
+}  
+
+module.exports = {
+  feed,
+}
